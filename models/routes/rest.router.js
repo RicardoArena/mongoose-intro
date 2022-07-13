227285c0e88d2a13ff7232e4f3f53b701add15a7
Ricardo Arena <arena_717@hotmail.com>
2022-07-13 16:54:00 -0300

    read done

--- models/routes/rest.router.js
+++ models/routes/rest.router.js
@@ -13,4 +13,29 @@ router.post("/creatrest", async (req, res) => {
   }
 });
 
+router.get("/all-rests", async (req, res) => {
+  try {
+    const allRests = await restModel.find();
+
+    return res.status(200).json(allRests);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const rest = await restModel.findOne({ _id: id });
+
+    return res.status(200).json(rest);
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
