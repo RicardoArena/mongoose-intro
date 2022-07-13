f0e2d86fca420d8d742a7c8ff5698f5a7a644664
Ricardo Arena <arena_717@hotmail.com>
2022-07-13 16:47:53 -0300

    creat done

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.json());
 const dbConnect = require("./config/db.config");
 dbConnect();
 
+const restRouter = require("./models/routes/rest.router");
+app.use("/rest", restRouter);
+
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and runing at port ${process.env.PORT}`);
 });
--- models/routes/rest.router.js
+++ models/routes/rest.router.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
 
 const restModel = require("../rest.model");
+
+router.post("/creatrest", async (req, res) => {
+  try {
+    const newRest = await restModel.create(req.body);
+
+    return res.status(201).json(newRest);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
